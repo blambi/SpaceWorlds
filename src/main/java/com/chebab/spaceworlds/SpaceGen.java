813559d0111fc4370c9e03ca9f8d80c15f97af8b
Patrik Lembke <blambi@chebab.com>
2012-09-23 01:48:12 +0200

    Inital

---
+++ src/main/java/com/chebab/spaceworlds/SpaceGen.java
@@ -0,0 +1,53 @@
+package com.chebab.spaceworlds;
+
+import java.util.Random;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Biome;
+import org.bukkit.generator.ChunkGenerator;
+
+public class SpaceGen extends ChunkGenerator {
+
+    /**
+     * A quite simple constructor don't you say?
+     */
+    SpaceGen() {
+    }
+
+    /**
+     * Generator :D
+     */
+    public byte[] generate( World world, Random rand, int chunkx, int chunkz ) {
+        byte[] blocks = new byte[65536];
+
+        int x, z;
+
+        // Since air is 0 it's allready filled with air,
+        if( chunkx == 0 && chunkz == 0 ) {
+            // Special since we need to take care of our spawn platform.
+
+            blocks[coordsToInt(1, 63, 1)] = (byte)Material.GLOWSTONE.getId();
+            for( x = 0; x < 3; x++ ) {
+                for( z = 0; z < 3; z++) {
+                    if( x == 1 && z == 1 )
+                        blocks[coordsToInt(x, 62, z)] = (byte)Material.BEDROCK.getId();
+                    else
+                        blocks[coordsToInt(x, 62, z)] = (byte)Material.GLOWSTONE.getId();
+                }
+            }
+            blocks[coordsToInt(1, 61, 1)] = (byte)Material.GLOWSTONE.getId();
+        }
+
+        return blocks;
+    }
+
+    private int coordsToInt( int x, int y, int z ) {
+        return ( x * 16 + z ) * 256 + y;
+    }
+
+    
+    @Override
+    public org.bukkit.Location getFixedSpawnLocation( World world, Random random ) {
+        return new org.bukkit.Location( world, 1, 64, 1 );
+    }
+}
---
+++ src/main/java/com/chebab/spaceworlds/SpaceWorlds.java
@@ -0,0 +1,25 @@
+package com.chebab.spaceworlds;
+
+import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SpaceWorlds extends JavaPlugin
+{
+
+    public void onEnable() {
+        System.out.println( "[Space Worlds] enabled." );
+    }
+
+    public void onDisable() {
+        System.out.println( "[Space Worlds] enabled." );
+    }
+
+    
+    @Override
+    public ChunkGenerator getDefaultWorldGenerator(String worldName, String id) {
+        /* Define our new generator... */
+        return new SpaceGen();
+    }
+}
